9b4ecc9b86b8705407da850009530a5a8d6524dd
zforgo <zforgo@gmail.com>
2015-09-05 17:13:35 +0200

    Initial

---
+++ release-poc/src/main/java/hu/zforgo/poc/App.java
@@ -0,0 +1,13 @@
+package hu.zforgo.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
